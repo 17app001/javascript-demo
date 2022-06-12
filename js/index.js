91a462e31203c0a7a4f59c7699addf9bffe22bbc
jerry <jerry@gmail.com>
2022-06-12 10:57:39 +0800

    樂透號碼產生器(1/2)

--- js/index.js
+++ js/index.js
@@ -1,32 +1,9 @@
-let a = false;
-total = 0;
-
-// while (true) {
-//     let x = prompt("輸入數字(q:exit)");
-//     if (x == "q") {
-//         break;
-//     }
-//     if (isNaN(x)) {
-//         console.log("輸入錯誤!");
-//         continue;
-//     }
-//     total += Number(x);
-//     console.log(total);
-// }
-
-// alert(`sum:${total}`);
-
-a = "30.123.34";
-
-console.log(Number(a));
-console.log(parseInt(a));
-console.log(parseFloat(a));
 
 
 // 函式表達式
 const getRandInt = (start, end) => Math.floor(Math.random() * (end - start + 1)) + start;
 
-console.log(getRandInt(10, 100));
+// console.log(getRandInt(10, 100));
 
 // 單一亂數function
 function getRandomNumber(start, end) {
@@ -59,13 +36,36 @@ function getLottoNumbers(start, end, count = 6, special = true) {
     return numbers;
 }
 
-let count = prompt("請輸入筆數:");
-let ulStr = "<h1>樂透號碼</h1><ul>";
+const dateEl = document.querySelectorAll(".date");
+const lottoEl = document.querySelector(".lotto-number ul");
+const startEl = document.querySelector("#start");
 
-for (let i = 0; i < count; i++) {
-    let lotto = getLottoNumbers(1, 49);
-    console.log(lotto);
-    ulStr += `<h2><li>${lotto.join(",")}</li></h2>`;
-}
-ulStr += "</ul>";
-document.write(ulStr);
+console.log(startEl);
+dateEl.forEach(date => {
+    date.innerText = "2022/6/12";
+});
+
+
+startEl.addEventListener("click", () => {
+    dateEl[0].innerHTML = new Date();
+
+    lottoEl.innerHTML = "";
+    for (let i = 0; i < 5; i++) {
+        let numbers = getLottoNumbers(1, 49, 6, true);
+        console.log(numbers);
+        lottoEl.innerHTML += `<li>${numbers.join(" ")}</li>`;
+    }
+
+    const liEl = document.querySelectorAll("li");
+
+    for (let i = 0; i < liEl.length; i++) {
+        liEl[i].style.color = "navy";
+        liEl[i].style.fontSize = "24px";
+        if (i % 2 == 0) {
+            liEl[i].style.background = "black";
+            liEl[i].style.color = "orange";
+
+        }
+
+    }
+});
